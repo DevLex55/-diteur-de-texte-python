078d3546ee49a2676bfe0678b644771910e9aa7d
DevLex55 <158478167+DevLex55@users.noreply.github.com>
2024-02-18 15:14:32 +0100

    fichier principale.py

--- README.py
+++ README.py
@@ -1,1 +1,8 @@
-print("hello world")
+print("bienvenu dans éditeur de texte")
+while True:
+  nv = input("nouveau documents ? (si oui faite 1 sinon autre)")
+  if nv == "1":
+    print("NOUVEAU DOCUMENT")
+    #la sa crée un nouveaux doc et ca met une page blanche écrivable
+  else:
+    print("ah mince, donc je sait plus quoi faire moi")
